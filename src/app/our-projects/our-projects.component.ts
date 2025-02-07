23f84f3446776d773a48d133f97ade6753791903
Istvan Kolumban <istvan.kolumban@metrixlab.com>
2025-02-07 14:48:21 +0200

    Our projects page: search functionality

--- src/app/our-projects/our-projects.component.ts
+++ src/app/our-projects/our-projects.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { expand } from 'rxjs';
 
 @Component({
   selector: 'app-our-projects',
@@ -76,6 +75,24 @@ export class OurProjectsComponent {
       expanded: false,
     }
   );
+
+  filteredProjects: Array<Project> = [...this.projects];
+
+  filterProjectsBySearch(event: Event): void {
+    const value = (event.target as HTMLInputElement).value.toLowerCase();
+    this.filteredProjects = this.projects.filter(
+      (project) =>
+        project.title.toLowerCase().includes(value) ||
+        project.year.toString().toLowerCase().includes(value) ||
+        project.month.toLowerCase().includes(value) ||
+        project.location.toLowerCase().includes(value) ||
+        project.participants.toLowerCase().includes(value) ||
+        project.organizers.some((organizer) =>
+          organizer.toLowerCase().includes(value)
+        ) ||
+        project.description.toLowerCase().includes(value)
+    );
+  }
 }
 
 export interface Project {
